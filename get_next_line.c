b2b8bf593dcf98448ecf0411e0cd5cd776f9994a
Chip <chipz68q@gmail.com>
2018-12-29 23:39:40 +0500

    Add test two line in two buf

--- get_next_line.c
+++ get_next_line.c
@@ -7,9 +7,13 @@ int		get_next_line(struct s_rz_file *file, char **line,
 						ssize_t (*pfread)(struct s_rz_file *, void *, size_t))
 {
 	static char	buf[BUFF_SIZE];
+	static char	buf_temp[BUFF_SIZE];
+	static int	buf_sz;
 	static int	pos = BUFF_SIZE;
+	int			ppos;
 	ssize_t		bytes_read;
 	char		*lf_pos;
+	int 		p_size;
 	struct s_rz_list *head;
 	struct s_rz_list *curr;
 	char		*p;
@@ -20,9 +24,16 @@ int		get_next_line(struct s_rz_file *file, char **line,
 
 	head = NULL;
 	list_n = 0;
+	buf_sz = 0;
 	if (pos < BUFF_SIZE)
 	{
 		lf_pos = ft_strchr(buf + pos, '\n');
+		if (lf_pos == NULL)
+		{
+			buf_sz = buf + BUFF_SIZE - (buf + pos);
+			memcpy(buf_temp, buf + pos, buf_sz);
+			goto r1;
+		}
 		bytes_read = lf_pos - (buf + pos);
 		*line = ft_strnew(bytes_read);
 		memcpy(*line, buf + pos, bytes_read);
@@ -48,7 +59,7 @@ int		get_next_line(struct s_rz_file *file, char **line,
 				memcpy(*line, buf, bytes_read);
 				return (1);
 			}
-			rz_list_add(&head, buf, bytes_read);
+			rz_list_add(&head, buf, BUFF_SIZE);
 			list_n++;
 			goto r1;
 		}
@@ -56,24 +67,39 @@ int		get_next_line(struct s_rz_file *file, char **line,
 		{
 			bytes_read = lf_pos - buf;
 			pos += bytes_read + 1;
-			if (head)
+			if (!head && buf_sz == 0)
 			{
-				p = ft_strnew(list_n * BUFF_SIZE + bytes_read);
-				curr = head;
-				pos = 0;
-				while (curr != NULL)
-				{
-					ft_memcpy(p + pos, curr->data, BUFF_SIZE);
-					pos += BUFF_SIZE;
-					curr = curr->next;
-				}
-				memcpy(p + pos, buf, bytes_read);				
-				rz_list_free(&head);
-				*line = p;
+				*line = ft_strnew(bytes_read);
+				ft_memcpy(*line, buf, bytes_read);
 				return (1);
 			}
-			*line = ft_strnew(bytes_read);
-			memcpy(*line, buf, bytes_read);
+			ppos = 0;
+			p_size = bytes_read;
+			if (buf_sz > 0)
+				p_size += buf_sz;
+			if (head)
+				p_size += list_n * BUFF_SIZE;
+			if (buf_sz || head)
+				p = ft_strnew(p_size);
+			if (buf_sz > 0)
+			{
+				ft_memcpy(p, buf_temp, buf_sz);
+				*line = p;
+				ppos += buf_sz;
+			}
+			if (head)
+			{
+				curr = head;
+				while (curr != NULL)
+				{
+					memcpy(p + ppos, curr->data, BUFF_SIZE);
+					ppos += BUFF_SIZE;
+					curr = curr->next;
+				}
+				rz_list_free(&head);
+			}
+			memcpy(p + ppos, buf, bytes_read);				
+			*line = p;
 		}
 	}
 	return (1);
--- tests/test_line.c
+++ tests/test_line.c
@@ -163,6 +163,34 @@ START_TEST(test_line_two_buf_one_line)
 }
 END_TEST
 
+START_TEST(test_line_two_buf_two_line)
+{
+	struct	s_rz_file	*file;
+	char				*line1;
+	char				*line2;
+	int					result1;
+	int					result2;
+	char				buf[] =
+	{
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '\n',
+		'A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '\n', EOF
+	};
+
+	file = rz_open_fd(1);
+	rz_set_read_buf(file, buf, sizeof buf);
+	line1 = line2 = NULL;
+	result1 = get_next_line(file, &line1, rz_read);
+	result2 = get_next_line(file, &line2, rz_read);
+	ck_assert_int_eq(result1, 1);
+	ck_assert_pstr_eq(line1, "0123456789");
+	ck_assert_int_eq(result2, 1);
+	ck_assert_pstr_eq(line2, "ABCDEF0123");
+	free(line1);
+	free(line2);
+	rz_close_fd(file);
+}
+END_TEST
+
 Suite *line_suite(void)
 {
 	Suite *s;
@@ -181,6 +209,7 @@ Suite *line_suite(void)
 	tcase_add_test(tc_line_one_buf, test_line_one_buf_two_line);
 	tcase_add_test(tc_line_one_buf, test_line_one_buf_three_line);
 	tcase_add_test(tc_line_one_buf, test_line_two_buf_one_line);
+	tcase_add_test(tc_line_one_buf, test_line_two_buf_two_line);
 
 	suite_add_tcase(s, tc_line_error_params);
 	suite_add_tcase(s, tc_line_one_buf);
