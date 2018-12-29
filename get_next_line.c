f11b0dfa9f28c87ca0e5d309a8c32c5d9a7cc84b
Chip <chipz68q@gmail.com>
2018-12-29 22:29:15 +0500

    Add test for 2 buf line

--- get_next_line.c
+++ get_next_line.c
@@ -10,10 +10,16 @@ int		get_next_line(struct s_rz_file *file, char **line,
 	static int	pos = BUFF_SIZE;
 	ssize_t		bytes_read;
 	char		*lf_pos;
+	struct s_rz_list *head;
+	struct s_rz_list *curr;
+	char		*p;
+	int			list_n;
 
 	if (file == NULL || file->fd < 0 || line == NULL)
 		return (-1);
 
+	head = NULL;
+	list_n = 0;
 	if (pos < BUFF_SIZE)
 	{
 		lf_pos = ft_strchr(buf + pos, '\n');
@@ -24,7 +30,7 @@ int		get_next_line(struct s_rz_file *file, char **line,
 		return (0);
 	}
 
-	bytes_read = pfread(file, buf, BUFF_SIZE);
+	r1:bytes_read = pfread(file, buf, BUFF_SIZE);
 	pos = 0;
 	if (bytes_read == 0)
 	{
@@ -34,11 +40,41 @@ int		get_next_line(struct s_rz_file *file, char **line,
 	else
 	{
 		lf_pos = ft_strchr(buf, '\n');
+		if (lf_pos == NULL)
+		{
+			if (bytes_read < BUFF_SIZE)
+			{
+				*line = ft_strnew(bytes_read);
+				memcpy(*line, buf, bytes_read);
+				return (1);
+			}
+			rz_list_add(&head, buf, bytes_read);
+			list_n++;
+			goto r1;
+		}
 		if (lf_pos != NULL)
+		{
 			bytes_read = lf_pos - buf;
-		pos += bytes_read + 1;
-		*line = ft_strnew(bytes_read);
-		memcpy(*line, buf, bytes_read);
+			pos += bytes_read + 1;
+			if (head)
+			{
+				p = ft_strnew(list_n * BUFF_SIZE + bytes_read);
+				curr = head;
+				pos = 0;
+				while (curr != NULL)
+				{
+					ft_memcpy(p + pos, curr->data, BUFF_SIZE);
+					pos += BUFF_SIZE;
+					curr = curr->next;
+				}
+				memcpy(p + pos, buf, bytes_read);				
+				rz_list_free(&head);
+				*line = p;
+				return (1);
+			}
+			*line = ft_strnew(bytes_read);
+			memcpy(*line, buf, bytes_read);
+		}
 	}
 	return (1);
 }
--- tests/test_line.c
+++ tests/test_line.c
@@ -141,6 +141,28 @@ START_TEST(test_line_one_buf_three_line)
 }
 END_TEST
 
+START_TEST(test_line_two_buf_one_line)
+{
+	struct	s_rz_file	*file;
+	char				*line;
+	int					result;
+	char				buf[] =
+	{
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '\n', EOF
+	};
+
+	file = rz_open_fd(1);
+	rz_set_read_buf(file, buf, sizeof buf);
+	line = NULL;
+	result = get_next_line(file, &line, rz_read);
+	ck_assert_int_eq(result, 1);
+	ck_assert_pstr_eq(line, "0123456789ABCDEF0123");
+	free(line);
+	rz_close_fd(file);
+}
+END_TEST
+
 Suite *line_suite(void)
 {
 	Suite *s;
@@ -158,6 +180,7 @@ Suite *line_suite(void)
 	tcase_add_test(tc_line_one_buf, test_line_one_buf_one_line_without_lf);
 	tcase_add_test(tc_line_one_buf, test_line_one_buf_two_line);
 	tcase_add_test(tc_line_one_buf, test_line_one_buf_three_line);
+	tcase_add_test(tc_line_one_buf, test_line_two_buf_one_line);
 
 	suite_add_tcase(s, tc_line_error_params);
 	suite_add_tcase(s, tc_line_one_buf);
