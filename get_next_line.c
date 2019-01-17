750f544e427659fd23d4473575275d2b7b115f27
Chip <chipz68q@gmail.com>
2019-01-18 00:37:55 +0500

    Opt1

--- get_next_line.c
+++ get_next_line.c
@@ -1,22 +1,10 @@
 #include <unistd.h>
+#include <stdio.h>
 #include "libft/libft.h"
 #include "get_next_line.h"
 #include "tests/read.h"
 
 static ssize_t (*pfread)(const int, void *, size_t) = rz_read;
-
-static int list_size(t_list *list)
-{
-	int size;
-
-	size = 0;
-	while (list)
-	{
-		list = list->next;
-		size++;
-	}
-	return (size);
-}
 
 static void del_node(void *p, size_t size)
 {
@@ -24,60 +12,32 @@ static void del_node(void *p, size_t size)
 	free(p);
 }
 
-static int cat(char **line, char *pbuf, t_list *bufs, char *sbuf, int lf_pos)
+static int ca(char **line, t_list *bufs, char *buf, int start, int end)
 {
 	t_list		*curr_buf;
 	char		*p;
-	int			pbuf_len;
 	int			line_len;
 
-	pbuf_len = ft_strlen(pbuf);
-	line_len = pbuf_len + list_size(bufs) * BUFF_SIZE + ft_strlen(sbuf);
-	if (!(p = ft_strnew(line_len)))
-	{
-		ft_lstdel(&bufs, del_node);
-		return (0);
-	}
-	*line = p;
-	p += pbuf_len + list_size(bufs) * BUFF_SIZE;
-	ft_memcpy(p, sbuf, lf_pos);
+	line_len = end - start;
 	curr_buf = bufs;
 	while (curr_buf)
 	{
-		p -= BUFF_SIZE;
-		ft_memcpy(p, curr_buf->content, BUFF_SIZE);
+		line_len += curr_buf->content_size;
 		curr_buf = curr_buf->next;
 	}
-	p = *line;
-	ft_memcpy(p, pbuf, pbuf_len);
-	ft_lstdel(&bufs, del_node);
+	if (!(p = ft_strnew(line_len)))
+		return (0);
+	*line = p;
+	p += line_len - (end - start);
+	ft_memcpy(p, buf + start, end - start);
+	curr_buf = bufs;
+	while (curr_buf)
+	{
+		p -= curr_buf->content_size;
+		ft_memcpy(p, curr_buf->content, curr_buf->content_size);
+		curr_buf = curr_buf->next;
+	}
 	return (1);
-}
-
-static int load_from_suffix_buf(char **line, char *pbuf, char *sbuf, int *pos)
-{
-	char			*lf;
-	int				line_len;
-	int				sbuf_len;
-
-	pbuf[0] = '\0';
-	if ((sbuf_len = ft_strlen(sbuf)) == 0 || *pos >= sbuf_len)
-		return (0);
-	lf = ft_strchr(sbuf + *pos, '\n');
-	if (lf)
-	{
-		line_len = lf - (sbuf + *pos);
-		*line = ft_strnew(line_len);
-		ft_memcpy(*line, sbuf + *pos, line_len);
-		*pos += line_len + 1;
-		return (1);
-	}
-	else
-	{
-		ft_strcpy(pbuf, sbuf + *pos);
-		*pos = 0;
-		return (0);
-	}
 }
 
 static ssize_t read_until_lf(int fd, t_list **bufs, char *sbuf, int *lf_pos)
@@ -109,29 +69,55 @@ static ssize_t read_until_lf(int fd, t_list **bufs, char *sbuf, int *lf_pos)
 	return (bytes_read);
 }
 
+static int	find_line_in_buf(const char *buf, int start, int *end)
+{
+	char	*lf;
+
+	*end = 0;
+	if (buf[start] == '\0')
+		return (0);
+	lf = ft_strchr(buf + start, '\n');
+	if (lf)
+	{
+		*end = lf - buf;
+		return (1);
+	}
+	else
+	{
+		*end = BUFF_SIZE;
+		return (0);
+	}
+}
+
 int		get_next_line(const int fd, char **line)
 {
-	static char		prefix_buf[BUFF_SIZE + 1];
-	static char		suffix_buf[BUFF_SIZE + 1];
-	static int		lf_pos;
-	t_list			*middle_bufs;
-	ssize_t			bytes_in_suffix_buf;
+	static char		buf[BUFF_SIZE + 1];
+	static int		start;
+	static int		end;
+	t_list			*lst;
+	int				bytes_in_suffix_buf;
 
 	if (fd < 0 || !line)
 		return (-1);
-	if (load_from_suffix_buf(line, prefix_buf, suffix_buf, &lf_pos))
-		return (1);
-	middle_bufs = 0;
-	bytes_in_suffix_buf = read_until_lf(fd, &middle_bufs, suffix_buf, &lf_pos);
-	if (bytes_in_suffix_buf == -1)
+	lst = 0;
+	bytes_in_suffix_buf = 0;
+	if (buf[0] == '\0' || !find_line_in_buf(buf, start, &end))
 	{
-		ft_lstdel(&middle_bufs, del_node);
+		if (buf[0] != '\0' && end != 0)
+			lst = ft_lstnew(buf + start, end - start);
+		start = 0;
+		bytes_in_suffix_buf = read_until_lf(fd, &lst, buf, &end);
+		if (bytes_in_suffix_buf == 0 && buf[0] == '\0' && !lst)
+			return (0);
+	}
+	if (bytes_in_suffix_buf == -1 || !ca(line, lst, buf, start, end))
+	{
+		if (lst)
+			ft_lstdel(&lst, del_node);
 		return (-1);
 	}
-	else if (bytes_in_suffix_buf == 0 && prefix_buf[0] == '\0' && !middle_bufs)
-		return (0);
-	if (!cat(line, prefix_buf, middle_bufs, suffix_buf, lf_pos))
-		return (-1);
-	lf_pos++;
+	if (lst)
+		ft_lstdel(&lst, del_node);
+	start = end + 1;
 	return (1);
 }
