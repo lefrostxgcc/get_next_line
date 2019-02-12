f1bcd83f5f017025f32d5f167831bf26ceca0ef7
Chip <chipz68q@gmail.com>
2019-02-12 23:20:36 +0500

    Add norm for gnl

--- get_next_line.c
+++ get_next_line.c
@@ -5,123 +5,89 @@
 
 static ssize_t (*pfread)(const int, void *, size_t) = rz_read;
 
+static int cl(ssize_t *start, ssize_t *bytes, char **line, int status)
+{
+	if (status <= 0)
+	{
+		if (status < 0)
+		{
+			free(*line);
+			*line = 0;
+		}
+		*bytes = 0;
+		*start = 0;
+		if (status == 0 && *line)
+			return (1);
+	}
+	return (status);
+}
+
+static int find_in_buf(char *buf, ssize_t *start, ssize_t *bytes, char **line)
+{
+	ssize_t		i;
+	ssize_t		len;
+
+	*line = 0;
+	len = 0;
+	if (*bytes <= 0)
+		return (0);
+	i = *start - 1;
+	while (++i < *bytes)
+		if (buf[i] == '\n')
+		{
+			if (!(*line = ft_strnew(i - *start)))
+				return (cl(start, bytes, line, -1));
+			memcpy(*line, buf + *start, i - *start);
+			*start = i + 1;
+			return (0);
+		}
+	if (i > *start)
+	{
+		len = i - *start;
+		if (!(*line = ft_strnew(len)))
+			return (cl(start, bytes, line, -1));
+		memcpy(*line, buf + *start, len);
+		return (len);
+	}
+	return (0);
+}
+
+static int cp(char *buf, ssize_t start, ssize_t *line_len, char **line)
+{
+	char	*p;
+
+	if (!(p = ft_strnew(*line_len + start)))
+		return (0);
+	memcpy(p, *line, *line_len);
+	memcpy(p + *line_len, buf, start);
+	free(*line);
+	*line = p;
+	*line_len += start;
+	return (1);
+}
+
 int		get_next_line(const int fd, char **line)
 {
-	static char buf[BUFF_SIZE];
-	static size_t start;
-	static ssize_t bytes;
-	char *p;
-	char *p2;
-	ssize_t i;
-	size_t len;
+	static char		b[BUFF_SIZE];
+	static ssize_t	i;
+	static ssize_t	n;
+	ssize_t			len;
 
 	if (fd < 0 || line == NULL)
 		return (-1);
-	len = 0;
-	p = 0;
-	p2 = 0;
-	if (bytes > 0)
-	{
-		i = start;
-		while (i < bytes)
-		{
-			if (buf[i] == '\n')
-			{
-				p = ft_strnew(i - start);
-				if (!p)
-				{
-					start = 0;
-					bytes = 0;
-					return (-1);
-				}
-				memcpy(p, buf + start, i - start);
-				*line = p;
-				start = i + 1;
-				return (1);
-			}
-			i++;
-		}
-		if (i == bytes && i > (ssize_t) start)
-		{
-			len = i - start;
-			p = ft_strnew(len);
-			if (!p)
-			{
-				start = 0;
-				bytes = 0;
-				return (-1);
-			}
-			memcpy(p, buf + start, len);
-		}
-	}
+	if ((len = find_in_buf(b, &i, &n, line)) == 0 && *line)
+		return (1);
 	while (1)
 	{
-		bytes = pfread(fd, buf, BUFF_SIZE);
-		if (bytes == -1)
-		{
-			free(p);
-			start = 0;
-			bytes = 0;
-			return (-1);
-		}
-		else if (bytes == 0)
-		{
-			start = 0;
-			if (p)
-			{
-				*line = p;
-				return (1);
-			}
-			else
-			{
-				return (0);
-			}
-		}
-		start = 0;
-		i = start;
-		while (i < bytes)
-		{
-			if (buf[i] == '\n')
-			{
-				p2 = ft_strnew(len + (i - start));
-				if (!p2)
-				{
-					free(p);
-					start = 0;
-					bytes = 0;
-					return (-1);
-				}
-				memcpy(p2, p, len);
-				memcpy(p2 + len, buf + start, i - start);
-				*line = p2;
-				free(p);
-				start = i + 1;
-				return (1);
-			}
-			i++;
-		}
-		if (i == bytes)
-		{
-			p2 = ft_strnew(len + (i - start));
-			if (!p2)
-			{
-				free(p);
-				start = 0;
-				bytes = 0;
-				return (-1);
-			}
-			memcpy(p2, p, len);
-			memcpy(p2 + len, buf + start, i - start);
-			len += i - start;
-			free(p);
-			p = p2;
-			if (bytes < BUFF_SIZE)
-			{
-				start = 0;
-				bytes = 0;
-				*line = p;
-				return (1);
-			}
-		}
+		if ((n = pfread(fd, b, BUFF_SIZE)) <= 0)
+			return (cl(&i, &n, line, n));
+		i = 0;
+		while (i < n)
+			if (b[i++] == '\n')
+				return (cp(b, i - 1, &len, line) ? 1 : cl(&i, &n, line, -1));
+		if (!cp(b, n, &len, line))
+			return (cl(&i, &n, line, -1));
+		if (n < BUFF_SIZE)
+			return (cl(&i, &n, line, 1));
 	}
 }
